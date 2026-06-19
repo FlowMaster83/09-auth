c9c6330bf437cf3525c4961c2688bd8175be93ab
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2026-06-19 13:27:47 +0300

    fixed

--- proxy.ts
+++ proxy.ts
@@ -28,10 +28,15 @@ export async function proxy(request: NextRequest) {
         const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
         for (const cookieStr of cookieArray) {
           const parsed = parse(cookieStr);
+          const options = {
+            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+            path: parsed.Path,
+            maxAge: Number(parsed['Max-Age']),
+          };
           if (parsed.accessToken)
-            cookieStore.set('accessToken', parsed.accessToken);
+            cookieStore.set('accessToken', parsed.accessToken, options);
           if (parsed.refreshToken)
-            cookieStore.set('refreshToken', parsed.refreshToken);
+            cookieStore.set('refreshToken', parsed.refreshToken, options);
         }
         if (isPublicRoute) {
           return NextResponse.redirect(new URL('/', request.url), {
